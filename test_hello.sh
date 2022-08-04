21931a6abbdcfe1888bcd1c432e9fe87494694e8
Shuntaro Kobayashi <z22012sk@aiit.ac.jp>
2022-08-04 20:49:57 +0900

    テスト

---
+++ test_hello.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ERROR_EXIT () {
+       	echo "$1" >&2 
+	rm -f /tmp/$$-* 
+	exit 1
+}
+
+./hello.sh > /tmp/$$-result
+echo "hello" > /tmp/$$-ans
+echo "world" >> /tmp/$$-ans
+diff /tmp/$$-ans /tmp/$$-result || ERROR_EXIT "result err"
+
+echo "テストクリア"
+rm -f /tmp/$$-*
